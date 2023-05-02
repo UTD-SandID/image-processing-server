897a2aa2064cdcd1e0ce536cb4ab84a58280bb7c
tmellon <50112750+tmellon@users.noreply.github.com>
2023-05-02 16:49:18 -0500

    Fixed firebase upload path
    Fixed relative path to not include local image path

--- src/utils/firebase_controller.py
+++ src/utils/firebase_controller.py
@@ -8,11 +8,7 @@ firebase_admin.initialize_app(cred, {'storageBucket': 'utdsand-id.appspot.com'})
 
 def firebase_image_upload(image_path):
     bucket = storage.bucket()
-    blob = bucket.blob(image_path)
+    blob_name = "SandImages/" + image_path.split("/")[-1]
+    blob = bucket.blob(blob_name)
     blob.upload_from_filename(image_path)
-
-#def store_firebase_image_url(image_url):
-#    ref = db.reference('images')
-#    ref.push({
-#        'url': image_url
-#    })
+    return blob.public_url
