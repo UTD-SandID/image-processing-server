2d16ba81d852688793c9e3452f27ce758cf7236a
Trent Mellor <50112750+tmellon@users.noreply.github.com>
2023-05-06 03:52:19 -0500

    Removed personal IP addresses used in testing
    These IP addresses point to the testing server in testing and are no longer needed. Plus its personal data that no longer needs to be seen.

--- src/config/settings/settings.py
+++ src/config/settings/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-u)idt&bid%pojf+va@1g!ncj-c)&bn$e+#4-nf4+o!(7%mwna)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.182', '75.12.150.23']
+ALLOWED_HOSTS = []
 
 
 # Application definition
